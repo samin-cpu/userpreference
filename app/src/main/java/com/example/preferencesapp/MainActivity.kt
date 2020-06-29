9a49ea558d3b05d79a4ecbc09402b01c3bb8e9b8
samin <a38207@alunos.ipb.pt>
2020-06-29 15:38:46 +0100

    UserInput

---
+++ app/src/main/java/com/example/preferencesapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.preferencesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.user_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.new_game -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                // start your next activity
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
